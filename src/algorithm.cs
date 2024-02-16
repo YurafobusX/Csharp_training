9eab09fd73fc2edebcba838cdf6264750f8232a7
YurafobusX <YurafobusX@gmail.com>
2024-02-16 14:16:30 +0500

    solution

--- src/algorithm.cs
+++ src/algorithm.cs
@@ -8,18 +8,21 @@ public static class Algrorithm {
 public static string? flipAndDuplex(string? input) {
     if (input == null) return null;
     var output = input.ToCharArray();
-    var error = new string("");
+    var extend = new string("");
     for (int i = 0; i < output.Length; i++) 
         if ((output[i] < 'a') || (output[i] > 'z')) 
-            error += $"Неверный символ \"{output[i]}\" на позиции {i+1}\n";
-    if (error.Length != 0) throw new Exception(error); 
+            extend += $"Неверный символ \"{output[i]}\" на позиции {i+1}\n";
+    if (extend.Length != 0) throw new Exception(extend); 
+    int[] chars = new int['z' - 'a' + 1];
+    foreach(var i in output) chars[i - 'a']++;
+    for (char i = 'a'; i <= 'z'; i++) extend += $"\n символ {i} всречался в изначальное строке {chars[i - 'a']} раз";
     if (input.Length % 2 == 0) {
         Array.Reverse(output, 0, output.Length / 2);
         Array.Reverse(output, output.Length / 2, output.Length / 2);
-        return new string(output);
+        return new string(output) + extend;
     } else {
         Array.Reverse(output);
-        return new string(output) + input;
+        return new string(output) + extend;
     }
 }  
 }
