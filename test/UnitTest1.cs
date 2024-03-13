2f5ccfd0c96365f630e53582c7fab149b1e48902
YurafobusX <YurafobusX@gmail.com>
2024-03-13 19:46:22 +0500

    beggin

---
+++ test/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace test;
+using NUnit.Framework;
+using Algrorithm;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.IsTrue("aaa" == "");
+    }
+}
